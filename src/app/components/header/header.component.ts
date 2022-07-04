0034acf3b905ef9653ccdd81d380421974d2d414
JulietaEsains <jesains@gmail.com>
2022-07-04 11:51:52 -0300

    mejor manejo de los nav links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Section, SECTIONS } from 'src/app/sections';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
+  sections: Section[] = SECTIONS
 
   constructor() { }
 
